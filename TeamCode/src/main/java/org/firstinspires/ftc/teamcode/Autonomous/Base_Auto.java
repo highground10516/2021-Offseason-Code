17a7b25dee3f5169916d6744214a75c6c251ae20
Ganesan <1758332@fcps.edu>
2020-03-04 10:05:47 -0500

    PIDF and Base_Auto Added

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Base_Auto.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.Autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+@Disabled
+@Autonomous(name = "Base Autonomous", group = "Autonomous")
+public class Base_Auto extends LinearOpMode {
+    // Create the robot
+    Robot robot = new Robot();
+    @Override
+    public void runOpMode() {
+        // Initialize Robot
+        robot.init(hardwareMap);
+        // Wait For Start To Be Pressed
+        waitForStart();
+        // Place all other commands below:
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Concept/PIDFConceptSkystone.java
@@ -0,0 +1,43 @@
+package org.firstinspires.ftc.teamcode.Concept;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.PIDCoefficients;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+@Autonomous(name = "Concept PIDF", group = "Concept")
+public class PIDFConceptSkystone extends LinearOpMode {
+    // Motor
+    DcMotorEx testMotor;
+    // PID Coefficients
+    public static final double k_P = 2.5;
+    public static final double k_I = 0.1;
+    public static final double k_D = 0.2;
+    public static final double k_F = 0f;
+
+    @Override
+    public void runOpMode() {
+        testMotor = (DcMotorEx)hardwareMap.get(DcMotor.class, "left_drive");
+
+        waitForStart();
+
+        PIDFCoefficients pidOrig = testMotor.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        PIDFCoefficients pidNew = new PIDFCoefficients(k_P, k_I, k_D, k_F);
+        testMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidNew);
+
+        PIDFCoefficients pidModified = testMotor.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        // display info to user.
+        while(opModeIsActive()) {
+            telemetry.addData("Runtime", "%.03f", getRuntime());
+            telemetry.addData("P,I,D,F (orig)", "%.04f, %.04f, %.04f, %.0f",
+                    pidOrig.p, pidOrig.i, pidOrig.d, pidOrig.f);
+            telemetry.addData("P,I,D,F (modified)", "%.04f, %.04f, %.04f, %.04f",
+                    pidModified.p, pidModified.i, pidModified.d, pidModified.f);
+            telemetry.update();
+        }
+    }
+}
